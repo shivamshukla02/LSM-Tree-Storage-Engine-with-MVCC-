f114079731c3e49768cdcd676a0c5e23a14ad7ca
Shivam Shukla <264753159+shivamshukla02@users.noreply.github.com>
2026-06-16 15:02:22 +0530

    Update Main.java
    Removed unnecessary tips for running code.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
 import java.net.StandardSocketOptions;
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String arg[]){
 MemTable memTable = new MemTable();
